083998cef055cb507d42fce2a38973eabc0d633b
brianthf <blora616@gmail.com>
2020-11-28 20:27:38 -0500

    add validation

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from datetime import date
 
+from cerberus import Validator
 
+
+# will calculate age based on current date - birthdate
 def calculate_age(birth_date):
     today = date.today()
     age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
@@ -8,6 +11,7 @@ def calculate_age(birth_date):
     return age
 
 
+# will parse user data from json and return User instance
 def parse_user(JSON):
     first_name = JSON.get('first_name', None)
     last_name = JSON.get('last_name', None)
@@ -18,12 +22,91 @@ def parse_user(JSON):
     return User(first_name, last_name, date.fromisoformat(birthdate), email)
 
 
+# will parse reading data from json and return Reading instance
 def parse_reading(JSON):
     weight = JSON.get('weight', None)
     blood_pressure = JSON.get('blood_pressure', None)
     temperature = JSON.get('temperature', None)
+    oxygen_level = JSON.get('oxygen_level', None)
     pulse = JSON.get('pulse', None)
+    datetime = JSON.get('date_time', None)
     user_id = JSON.get('user_id', None)
 
     from models import Reading
-    return Reading(weight, blood_pressure, temperature, pulse, user_id)
+    return Reading(weight, blood_pressure, temperature, oxygen_level, pulse, datetime, user_id)
+
+
+# will parse activity data from json and return Activity instance
+def parse_activity(JSON):
+    type = JSON.get('type', None)
+    duration = JSON.get('duration', None)
+    description = JSON.get('description', None)
+    datetime = JSON.get('datetime', None)
+    user_id = JSON.get('user_id', None)
+
+    from models import Activity
+    return Activity(type, duration, description, datetime, user_id)
+
+
+user_schema = {
+    'first_name': {
+        'type': 'string'
+    },
+    'last_name': {
+        'type': 'string'
+    },
+    'birthdate': {
+        'type': 'date'
+    },
+    'email': {
+        'type': 'string'
+    }
+}
+
+user_validator = Validator(user_schema)
+
+reading_schema = {
+    'weight': {
+        'type': 'number'
+    },
+    'blood_pressure': {
+        'type': 'number'
+    },
+    'temperature': {
+        'type': 'number'
+    },
+    'oxygen_level': {
+        'type': 'number'
+    },
+    'pulse': {
+        'type': 'number'
+    },
+    'datetime': {
+        'type': 'datetime'
+    },
+    'user_id': {
+        'type': 'number'
+    }
+}
+
+reading_validator = Validator(reading_schema)
+
+activity_schema = {
+    'type': {
+        'type': 'string'
+    },
+    'duration': {
+        'type': 'string'
+    },
+    'description': {
+        'type': 'string'
+    },
+    'datetime': {
+        'type': 'datetime'
+    },
+    'user_id': {
+        'type': 'number'
+    }
+}
+
+activity_validator = Validator(activity_schema)
--- views/main.py
+++ views/main.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, request, jsonify
+
 from app import db
 from models import User
-from utils import parse_user
+from utils import (parse_user, parse_reading, parse_activity, activity_validator, reading_validator, user_validator)
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/users', methods=['POST'])
 def create_user():
+    if not user_validator.validate(request.json):
+        raise ValueError()
     data = parse_user(request.json)
 
     user = User.query.filter_by(email=data.email).first()
@@ -21,6 +24,39 @@ def create_user():
     return jsonify(user=data.serialize()), 201
 
 
-@main.route('/users/readings', methods=['POST'])
-def create_reading():
-    pass
+@main.route('/readings/user/<int: id>', methods=['POST'])
+def create_reading(id):
+    if not reading_validator.validate(request.json):
+        raise ValueError(reading_validator.errors)
+
+    reading = parse_reading(request.json)
+
+    user = User.query.filter_by(user_id=id).first()
+
+    if user is None:
+        raise LookupError("Sorry, that user does not exist.")
+
+    reading.user_id = id
+    db.session.add(reading)
+    db.session.commit()
+
+    return jsonify(reading=reading.serialize()), 201
+
+
+@main.route('/activities/user/<int: id>', methods=['POST'])
+def create_activity(id):
+    if not activity_validator.validate(request.json):
+        raise ValueError(activity_validator.errors)
+
+    activity = parse_activity(request.json)
+
+    user = User.query.filter_by(user_id=id).first()
+
+    if user is None:
+        raise LookupError("Sorry, that user does not exist.")
+
+    activity.user_id = id
+    db.session.add(activity)
+    db.session.commit()
+
+    return jsonify(activity=activity.serialize()), 201
