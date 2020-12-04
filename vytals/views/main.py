579abb151423b2a8bbd805ecf494f4d85a171ed3
brianthf <blora616@gmail.com>
2020-12-04 14:45:11 -0500

    role

--- vytals/views/main.py
+++ vytals/views/main.py
@@ -2,8 +2,8 @@ from flask import Blueprint, request, jsonify
 
 from vytals import db
 from vytals.exceptions import InvalidUsage
-from vytals.models import User
-from vytals.utils import parse_user, user_validator
+from vytals.models import User, Role
+from vytals.utils import parse_user, user_validator, role_required
 
 main = Blueprint('main', __name__)
 
@@ -14,17 +14,21 @@ def create_user():
         raise InvalidUsage(user_validator.errors, status_code=422)
 
     data = parse_user(request.json)
+
     user = User.query.filter_by(username=data.username).first()
+    role = Role.query.get(1)  # 1 will always represent the USER role
 
     if user:
         raise InvalidUsage("That user already exists in the system.", status_code=409)
 
+    data.roles.append(role)
     db.session.add(data)
     db.session.commit()
     return jsonify(user=data.serialize()), 201
 
 
 @main.route('/users/all', methods=['GET'])
+@role_required("ADMIN")
 def get_all_users():
     users = User.query.all()
     return jsonify(users=[u.serialize() for u in users]), 200
