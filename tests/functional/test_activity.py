2c29e113e2e9eacd242bb1f06f42ad136d64a5fb
brianthf <blora616@gmail.com>
2020-12-17 20:33:13 -0500

    activities/invalid user id test

--- tests/functional/test_activity.py
+++ tests/functional/test_activity.py
@@ -32,3 +32,10 @@ def test_create_activity_with_invalid_user(test_client, init_database):
     assert response.status_code == 404
     assert b"There is no user associated with the id provided." in response.data
     assert b"activity" not in response.data
+
+
+def test_get_activities_with_invalid_user(test_client, init_database):
+    response = test_client.get("/activities/user/123/all")
+
+    assert response.status_code == 404
+    assert b"There are no activities associated with the user id provided." in response.data
