8fa7f9200ef4c9cccec677e723c0a50f0f3e02b1
brianthf <blora616@gmail.com>
2020-12-17 22:55:43 -0500

    test_to_datetime

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,4 +1,4 @@
-from vytals.utils import to_date
+from vytals.utils import to_date, to_date_time, test_email_address
 from datetime import datetime, date
 
 
@@ -7,3 +7,11 @@ def test_successful_to_date():
 
     assert isinstance(valid_date, date) is True
     assert valid_date
+
+
+def test_successful_to_date_time():
+    valid_datetime = to_date_time("2015-03-25T11:00:00")
+
+    assert isinstance(valid_datetime, datetime) is True
+    assert valid_datetime
+
