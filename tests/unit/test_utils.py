c583a613fa10907e8bf488cebf85557ca3e7beda
brianthf <blora616@gmail.com>
2020-12-19 17:57:38 -0500

    fix utils-test

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -5,19 +5,27 @@ from datetime import datetime, date
 def test_successful_to_date():
     valid_date = to_date("2001-11-14")
 
-    assert isinstance(valid_date, date) is True
     assert valid_date
+    assert isinstance(valid_date, date) is True
+    assert valid_date.month == 11
+    assert valid_date.day == 14
+    assert valid_date.year == 2001
 
 
 def test_successful_to_date_time():
     valid_datetime = to_date_time("2015-03-25T11:00:00")
 
-    assert isinstance(valid_datetime, datetime) is True
     assert valid_datetime
+    assert isinstance(valid_datetime, datetime) is True
+    assert valid_datetime.day == 25
+    assert valid_datetime.hour == 11
+    assert valid_datetime.month == 3
+    assert valid_datetime.year == 2015
 
 
 def test_successful_email_address_input():
     valid_email = email_validator("test@gmail.com")
 
-    assert isinstance(valid_email, str)
     assert valid_email
+    assert isinstance(valid_email, str)
+
